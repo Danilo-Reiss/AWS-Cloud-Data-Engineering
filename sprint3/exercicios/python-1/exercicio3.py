a769b9811430010c4c99aec89eb01affdb882f9d
Danilo Reis <142188792+Danilo-Reiss@users.noreply.github.com>
2024-05-06 00:20:22 -0300

    Delete sprint3/exercicios/python-1 directory

--- sprint3/exercicios/python-1/exercicio3.py
+++
@@ -1,6 +0,0 @@
-""" Escreva um código Python para imprimir os números pares de 0 até 20 (incluso).
-Importante: Aplique a função range() em seu código. """
-
-for n in range(0,21):
-    if n % 2 == 0:
-        print(f'{n}')
