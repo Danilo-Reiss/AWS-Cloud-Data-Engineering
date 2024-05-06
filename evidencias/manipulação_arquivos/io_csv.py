0d33e1c1db6aa0310234cbca84dcf41afb73c2b1
Danilo Reis <142188792+Danilo-Reiss@users.noreply.github.com>
2024-05-06 00:30:57 -0300

    Delete evidencias directory

--- evidencias/manipulação_arquivos/io_csv.py
+++
@@ -1,5 +0,0 @@
-import csv
-
-with open('pessoas.csv') as entrada:
-    for pessoa in csv.reader(entrada): # --> csv.reader já faz todo os tratamentos dos dados
-        print('Nome: {}, Idade: {}'.format(*pessoa))
--- evidencias/manipulação_arquivos/io_v1.py
+++
@@ -1,7 +0,0 @@
-#! python
-arquivo = open('pessoas.csv')
-dados = arquivo.read()
-arquivo.close()
-
-for registro in dados.splitlines():
-    print('Nome: {}, Idade: {}'.format(*registro.split(','))) # --> * vai separar os argumentos da list nas variáveis
